89fee5a48ee29a1d43b0f3b397390d917bb1f960
hamsahesham <h.hamssa66@gmail.com>
2025-03-14 14:33:57 +0200

    change

--- config/dataBase.js
+++ config/dataBase.js
@@ -1,10 +1,12 @@
 import { Sequelize } from 'sequelize';
+import mysql2 from 'mysql2';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: "config.env" });
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,process.env.DB_PASS, {
   host: process.env.DB_HOST,
     dialect: "mysql",
+    dialectModule: mysql2,
     timezone: "+02:00", 
     logging: false,
 });

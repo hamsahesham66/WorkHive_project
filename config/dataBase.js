39b377294aa3ff8824f7ee8f35bae857b9f1e7dc
hamsahesham <h.hamssa66@gmail.com>
2025-03-14 14:27:22 +0200

    try

--- config/dataBase.js
+++ config/dataBase.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: "config.env" });
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,process.env.DB_PASS, {
-    host: "localhost", 
+  host: process.env.DB_HOST,
     dialect: "mysql",
     timezone: "+02:00", 
     logging: false,

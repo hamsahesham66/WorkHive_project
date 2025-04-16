3f2f5a8884d7302053b8d4a270e42aa5d47c40b7
hamsahesham <h.hamssa66@gmail.com>
2025-04-16 20:43:43 +0200

    change category model

--- models/categoriesModel.js
+++ models/categoriesModel.js
@@ -22,6 +22,11 @@ const Category = sequelize.define(
       type: DataTypes.STRING,
       allowNull: true,
     },
+    static_image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    
     created_at: {
       type: DataTypes.DATE,
       allowNull: false,

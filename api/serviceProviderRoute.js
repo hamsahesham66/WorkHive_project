8644e54aeed5f629f8e1104b8b86a9e6e96d5d24
hamsahesham <h.hamssa66@gmail.com>
2025-04-23 20:50:00 +0200

    add new change

--- api/serviceProviderRoute.js
+++ api/serviceProviderRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { getServiceProvidersByCategory } from "../services/serviceProviderService.js";
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 // Route to get service providers by category
 router.get("/categories/:categoryId/service-providers", getServiceProvidersByCategory);
